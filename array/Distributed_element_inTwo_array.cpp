a84909704d4ce2fe0b0d65b8d3c9a1993612a68d
nirbcodes <nirbhaychukekar21@gmail.com>
2026-08-20 23:45:26 +0530

    Add files via upload

---
+++ array/Distributed_element_inTwo_array.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
+
+        vector<int>arr1;
+        vector<int>arr2;
+
+        int n=nums.size();
+        
+
+        
+
+        for (int i=0;i<n;i++){
+
+            if(i==0){
+
+                arr1.push_back(nums[i]);
+               
+            }
+            else if(i==1){
+                 arr2.push_back(nums[i]);
+            }
+            else if ( arr1.back()>arr2.back()){
+
+                arr1.push_back(nums[i]);
+            }
+             else {
+
+                arr2.push_back(nums[i]);
+            }
+
+
+        
+            
+
+           
+             
+
+
+
+            
+        }
+        vector<int> result = arr1;
+         result.insert(result.end(), arr2.begin(), arr2.end());
+
+           
+
+         return result;
+ 
+        
+    }
+};
